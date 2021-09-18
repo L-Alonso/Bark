c7d0158c8b45cc66cf4985a1e6e09137aa5b4556
Shayne Lemons <lemonsshayne6@gmail.com>
2021-09-18 11:02:37 -0700

    create js variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var searchParkEl = $("#park-search")
+var natParkNameEl = $("#nat-park-name");
+var natParkInfoEl = $("#nat-park-info");
+var natParkMapEL = $("#nat-park-map");
+var searchBtn = $("#search-btn");
